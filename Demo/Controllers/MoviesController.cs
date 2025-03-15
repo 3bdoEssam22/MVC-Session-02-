b4805ba1f738060c6ff01cfe4dcacfa021a2f80b
3bdoEssam22 <abdulrahman.e.f22@gmail.com>
2025-03-16 00:59:25 +0300

    Add project files.

---
+++ Demo/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //BaseURL/Movies/Index
+        public string Index()
+        {
+            return ("Hello from Index");
+        }
+
+        //BaseURL/Movies/GetMovies?id=10&name=Film
+        [HttpGet]
+        public string GetMovies(int? id, string name)
+        {
+            return $"Movie Id: {id}, Name: {name}";
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseRouting();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.MapControllerRoute(
+                    name: "Default",
+                    pattern: "{controller=Movies}/{action=Index}"
+                    );
+            //defaults: new { action = "Index", controller = "Movies" },
+            //constraints: new { Id = @"\d{2}"});
+
+
+            //app.MapGet("/X{name}", async context =>
+            //{
+            //    //var Name = context.GetRouteValue("name");
+            //    await context.Response.WriteAsync($"Hello {context.Request.RouteValues["name"]}!");
+            //});
+
+
+            app.Run();
+        }
+    }
+}
